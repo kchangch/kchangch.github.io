6cdbd54a7890e574461d6577c45a5d602a69bd21
kchangch <kidjou_chang_chu@sfu.ca>
2022-04-20 01:09:06 -0700

    wip

--- components/Table.js
+++ components/Table.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { useState } from "react";
 
 const Table = ({ tableHeader, locations }) => {
+	console.log(locations);
+
 	const [data, setData] = useState(locations);
 	const [order, setOrder] = useState("ASC");
 
